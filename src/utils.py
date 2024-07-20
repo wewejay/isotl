1ac13f8b388d2a7373bb61f3dccd98403a1b4a34
wewejay <glgleemolole@163.com>
2024-07-20 20:42:57 +0200

    Unmounting for Windows

--- src/utils.py
+++ src/utils.py
@@ -11,13 +11,13 @@ def ps_run(cmd: str) -> subprocess.CompletedProcess:
     return completed
 
 
-def mount_win(abspath: str) -> tuple[str, str] | None:
+def mount_win(abs_path: str) -> tuple[str, str] | None:
     """
     Mount an ISO file in Windows
-    :param abspath: Absolute path of the ISO file
+    :param abs_path: Absolute path of the ISO file
     :return: (Drive letter, File system label)
     """
-    cmd = (f"$result = Mount-DiskImage \"{abspath}\" -PassThru ;" +
+    cmd = (f"$result = Mount-DiskImage \"{abs_path}\" -PassThru ;" +
            "$drive = ($result | Get-Volume) ;" +
            "Write-Output $drive.DriveLetter ;" +
            "Write-Output $drive.FileSystemLabel")
@@ -31,3 +31,19 @@ def mount_win(abspath: str) -> tuple[str, str] | None:
     drive = output_lines[0].strip()
     label = output_lines[1].strip()
     return drive, label
+
+
+def unmount_win(abs_path: str) -> bool:
+    """
+    Dismount an ISO file in Windows
+    :param abs_path: Absolute path of the ISO file
+    :return: True if successful, False otherwise
+    """
+    cmd = f"Dismount-DiskImage \"{abs_path}\""
+    raw_output = ps_run(cmd)
+    output = raw_output.stdout.decode("utf-8").strip()
+    error = raw_output.stderr.decode("utf-8").strip()
+    if error != "":
+        print(error)
+        return False
+    return True
